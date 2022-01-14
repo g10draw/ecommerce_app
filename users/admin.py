2b18570f39549ee233b33ab2e17854a17d276169
g10draw <mjitendrareddy@gmail.com>
2022-01-14 18:29:41 +0530

    Revert "initial commit"
    This reverts commit 5550d1f62f4896b18506bd28abbaa8e78c5a0aa8.

--- users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-# Register your models here.
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    """ Registers Profile Object to Admin """
-    list_display = ['user', 'phone_number', 'address', 'postal_code', 'city']
